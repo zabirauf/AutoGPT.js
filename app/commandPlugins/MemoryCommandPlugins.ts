d6a681e44d7310c02a9e9dc119165df8e9dfd723
Zohaib Rauf <zabirauf@gmail.com>
2023-04-05 00:18:40 -0700

    Transitioned to plugin infra and added File Operations as well

---
+++ app/commandPlugins/MemoryCommandPlugins.ts
@@ -0,0 +1,62 @@
+import { CommandPlugin } from "./CommandPlugin";
+
+const permanentMemory: string[] = [];
+
+function commitMemory(val: string) {
+  permanentMemory.push(val);
+
+  return `Committing memory with string "${val}"`;
+}
+function getMemory(index: number) {
+  if (index >= permanentMemory.length) {
+    return "Invalid key, cannot retrieve memory.";
+  }
+  return permanentMemory[index];
+}
+
+function deleteMemory(index: number) {
+  if (index >= permanentMemory.length) {
+    return "Invalid key, cannot delete memory.";
+  }
+
+  permanentMemory.splice(index, 1);
+  return `Deleting memory with key ${index}.`;
+}
+
+function overwriteMemory(index: number, val: string) {
+  if (index >= permanentMemory.length) {
+    return "Invalid key, cannote overwrite memory.";
+  }
+
+  permanentMemory[index] = val;
+  return `Overwriting memory with key ${index} and string "${val}".`;
+}
+
+const MemoryCommandPlugins: CommandPlugin[] = [
+  {
+    command: "memory_add",
+    name: "Memory Add",
+    arguments: {
+      string: "string",
+    },
+    execute: async (args) => commitMemory(args["string"]),
+  },
+  {
+    command: "memory_del",
+    name: "Memory Delete",
+    arguments: {
+        key: "key"
+    },
+    execute: async (args) => deleteMemory(parseInt(args["key"]))
+  },
+  {
+    command: "memory_ovr",
+    name: "Memory Overwrite",
+    arguments: {
+        key: "key",
+        string: "string"
+    },
+    execute: async (args) => overwriteMemory(parseInt(args["key"]), args["string"])
+  },
+];
+export default MemoryCommandPlugins;
