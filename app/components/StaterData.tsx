21a28446c418bdeedb0db0576f65cf320d83f925
Zohaib Rauf <zabirauf@gmail.com>
2023-04-06 21:48:06 -0700

    Added some basic components

---
+++ app/components/StaterData.tsx
@@ -0,0 +1,65 @@
+export function StarterData() {
+  return (
+    <form>
+      <div className="space-y-12">
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            Profile
+          </h2>
+          <p className="mt-1 text-sm leading-6 text-gray-600">
+            This information will be displayed publicly so be careful what you
+            share.
+          </p>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Username
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
+                    workcation.com/
+                  </span>
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    autoComplete="username"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="janesmith"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label
+                htmlFor="about"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                About
+              </label>
+              <div className="mt-2">
+                <textarea
+                  id="about"
+                  name="about"
+                  rows={3}
+                  className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
+                  defaultValue={""}
+                />
+              </div>
+              <p className="mt-3 text-sm leading-6 text-gray-600">
+                Write a few sentences about yourself.
+              </p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,11 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { CommandPlugins } from "~/commandPlugins";
 import { chatWithAI } from "~/utils/chat";
 import { generatePrompt } from "~/utils/prompt";
-import { countStringTokens } from '~/utils/tokenCounter';
+import { countStringTokens } from "~/utils/tokenCounter";
+import { TokenRequest } from "~/components/TokenRequest";
+import { getAPIKey } from "~/utils/apiKey";
+import { StarterData } from '~/components/StaterData';
 
 export default function Index() {
   useEffect(() => {
@@ -11,12 +14,19 @@ export default function Index() {
     console.log(generatePrompt());
     console.log(countStringTokens("Hello world", "gpt-3.5-turbo-0301"));
   }, []);
+
+  const [hasToken, setHasToken] = useState<boolean>(false);
+  useEffect(() => setHasToken(!!getAPIKey()), []);
+  const onTokenSaved = useCallback(() => {
+    setHasToken(!!getAPIKey());
+  }, []);
+
   return (
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
       <div className="relative sm:pb-16 sm:pt-8">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
-            <h1>Hello world!</h1>
+            {!hasToken ? <TokenRequest onTokenSaved={onTokenSaved} /> : <StarterData />}
           </div>
         </div>
       </div>
